077fc73d567f762b9d162b4e7b14ed59f9a13f47
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-03 17:10:04 +0700

    set tz to ict

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-from pathlib import Path
 import sys
-ROOT_FOLDER_LOCATION = Path(__file__).resolve().parents[0]
+from pathlib import Path
+ROOT_FOLDER_LOCATION = Path(__file__).resolve().parents[1]
 sys.path.append(str(ROOT_FOLDER_LOCATION))
 
 from datetime import datetime, timedelta
@@ -31,7 +31,7 @@ def main():
     """
     Main Budget Reconciliation entrypoint
     ---
-    Workflow:
+    Principles:
         1. Resolve execution time window from MODE
         2. Validate OS environment variables
         3. Load secrets from GCP Secret Manager
